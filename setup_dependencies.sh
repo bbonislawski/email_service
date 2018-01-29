44606c99a00ba01db817bc56862b7cdc2f8f7c2c
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2018-01-29 16:17:11 +0100

    add script to setup dependencies on development machine

---
+++ setup_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+go get github.com/sendgrid/sendgrid-go
+go get github.com/sendgrid/sendgrid-go/helpers/mail
+go get github.com/aws/aws-lambda-go/events
+go get github.com/aws/aws-lambda-go/lambda
+go get github.com/satori/go.uuid
+go get github.com/rubyist/circuitbreaker
+go get gopkg.in/mailgun/mailgun-go.v1
+go get github.com/aws/aws-sdk-go/aws
+go get github.com/aws/aws-sdk-go/aws/session
+go get github.com/aws/aws-sdk-go/service/dynamodb
+go get github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute
