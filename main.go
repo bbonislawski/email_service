095b9447c817b38e15dcc0e28b5ad804c09fa55a
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2018-01-29 16:19:33 +0100

    send emails from dynamodb events

--- main.go
+++ main.go
@@ -11,12 +11,19 @@ type Adapter interface {
 }
 
 func Handler(ctx context.Context, e events.DynamoDBEvent) {
+  for _, record := range e.Records {
+    if record.EventName == "INSERT" {
+      to := record.Change.NewImage["Address"].String()
+      subject := record.Change.NewImage["Subject"].String()
+      content := record.Change.NewImage["Content"].String()
+      sendEmail(to, subject, content)
+    }
+  }
 }
 
 func sendEmail(to, subject, content string) (string, error) {
   var a Adapter = MailgunAdapter{}
   result, err := a.SendEmail(to, subject, content)
-
   return result, err
 }
 
